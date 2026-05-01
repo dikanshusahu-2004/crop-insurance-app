55210671f1b03610e549a95c27a83e893f1a9265
dikanshu <sdikanshu@gmail.com>
2026-05-01 16:33:25 +0530

    update full claim form backend

--- backend/models/Claim.js
+++ backend/models/Claim.js
@@ -1,8 +1,13 @@
 const mongoose = require("mongoose");
 const Claim = require("../models/Claim");
-
-router.post("/", async (req, res) => {
+const multer = require("multer");
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+router.post("/", upload.single("damage_image"), async (req, res) => {
   try {
+
+    console.log("BODY:", req.body);
+    console.log("FILE:", req.file);
 
     const newClaim = new Claim({
 
@@ -27,18 +32,19 @@ router.post("/", async (req, res) => {
       ifsc_code: req.body.ifsc_code,
 
       policy_number: req.body.policy_number,
-      sum_insured: req.body.sum_insured
+      sum_insured: req.body.sum_insured,
+
+      image: req.file ? req.file.originalname : ""
 
     });
 
     await newClaim.save();
 
-    res.json({ message: "Full Claim Saved ✅" });
+    res.json({ message: "Saved ✅" });
 
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: err.message });
   }
 });
-
 module.exports = mongoose.model("Claim", ClaimSchema);
