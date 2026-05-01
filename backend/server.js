aab5f771c84541d8cd042d5d064de8d588047149
dikanshu <sdikanshu@gmail.com>
2026-05-01 11:39:22 +0530

    fix frontend route

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-require('dotenv').config();   // ✅ सबसे ऊपर
+const path = require("path");
+
+require("dotenv").config();
 
 const app = express();
 
@@ -9,18 +11,23 @@ app.use(cors());
 app.use(express.json());
 
 console.log("MONGO URI:", process.env.MONGO_URI);
-// ✅ MongoDB connect (ONLY ONCE)
+
+// MongoDB connect
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected ✅"))
   .catch(err => console.log(err));
 
-// routes
+// API routes
 app.use("/api/app", require("./routes/app"));
 app.use("/api/claim", require("./routes/claim"));
 app.use("/api/auth", require("./routes/auth"));
 
+// ✅ frontend serve
+app.use(express.static(path.join(__dirname, "../frontend")));
+
+// ✅ homepage
 app.get("/", (req, res) => {
-  res.send("API Running 🚀");
+  res.sendFile(path.join(__dirname, "../frontend/index.html"));
 });
 
 const PORT = process.env.PORT || 5000;
